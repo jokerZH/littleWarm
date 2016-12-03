eaaec9161fcacf55c00c4a7d00ca03bcea80176e
zhonghua <zhonghua@weidian.com>
2016-12-03 22:01:49 +0800

    init

---
+++ src/main/java/com/joker/servlet/SessionServlet.java
@@ -0,0 +1,19 @@
+package com.joker.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SessionServlet {
+
+    @RequestMapping(value="/test",produces="application/json; charset=utf-8")
+    @ResponseBody
+    public String startSession(HttpServletRequest request) {
+        String jobName = request.getParameter("hehe");
+
+        return "hello";
+    }
+}
